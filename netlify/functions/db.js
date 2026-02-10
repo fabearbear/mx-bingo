4c06c716ae262def3f985c17567a62d6dce5e596
fabearbear <fa.bearbear@gmail.com>
2026-02-11 00:26:39 +0800

    Create db.js

---
+++ netlify/functions/db.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+let pool;
+
+function getPool() {
+  if (!pool) {
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      },
+      max: 5,
+      idleTimeoutMillis: 30000,
+      connectionTimeoutMillis: 5000,
+    });
+  }
+  return pool;
+}
+
+module.exports = { getPool };
